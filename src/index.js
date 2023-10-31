f8d442942aaab3d3e5aa2bda0130ddb939bf65d5
Javad Mnjd <javad.mnjd@hotmail.com>
2023-10-31 13:02:51 +0330

    fix: remove utilities supported by latest version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 const plugin = require("tailwindcss/plugin");
-
-const paddingUtilities = require("./paddingUtilities");
-const marginUtilities = require("./marginUtilities");
-const floatUtilities = require("./floatUtilities");
-const insetUtilities = require("./insetUtilities");
-const clearUtilities = require("./clearUtilities");
-const borderRadiusUtilities = require("./borderRadiusUtilities");
-const borderWidthUtilities = require("./borderWidthUtilities");
+ 
+const floatUtilities = require("./floatUtilities"); 
+const clearUtilities = require("./clearUtilities"); 
 const textAlignUtilities = require("./textAlignUtilities");
 const transformOriginUtilities = require("./transformOriginUtilities");
 const spaceUtilities = require("./spaceUtilities");
@@ -14,14 +9,10 @@ const divideUtilities = require("./divideUtilities");
 const backgroundAlignUtilities = require('./backgroundAlignUtilities');
 
 module.exports = plugin((helpers) => {
-  const { addUtilities, matchUtilities, theme, variants } = helpers;
-  paddingUtilities(helpers);
-  marginUtilities(helpers);
-  insetUtilities(helpers);
+  const { addUtilities, variants } = helpers; 
   borderRadiusUtilities(helpers);
   addUtilities(clearUtilities(helpers), variants("clear"));
-  addUtilities(floatUtilities(helpers), variants("float"));
-  borderWidthUtilities(helpers);
+  addUtilities(floatUtilities(helpers), variants("float")); 
   addUtilities(textAlignUtilities(helpers), variants("textAlign"));
   addUtilities(transformOriginUtilities(), variants("transformOrigin"));
   spaceUtilities(helpers);
